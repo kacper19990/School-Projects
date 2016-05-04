839565c9f5d1a89c7786d7b1c4c4c880bfe5c67f
Kacper Maczka <kacper19990@hotmail.com>
2016-05-04 17:47:59 +0100

    Updated LED code
    Rewrote the code from the start, which allows us to have the traffic
    lights run in the correct order

--- Traffic Lights.py
+++ Traffic Lights.py
@@ -1,137 +1,42 @@
-<<<<<<< HEAD
-import RPi.GPIO as GPIO
-import time
-
-def RL1():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(58,GPIO.OUT)
-	GPIO.output(58,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(58,GPIO.LOW)
-
-def RL2():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(57,GPIO.OUT)
-	GPIO.output(57,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(57,GPIO.LOW)
-
-def OL1():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(22,GPIO.OUT)
-	GPIO.output(22,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(22,GPIO.LOW)
-
-def OL2():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(23,GPIO.OUT)
-	GPIO.output(23,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(23,GPIO.LOW)
-	
-def GL1():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(6,GPIO.OUT)
-	GPIO.output(6,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(6,GPIO.LOW)
-	
-def GL2():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(52,GPIO.OUT)
-	GPIO.output(52,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(52,GPIO.LOW)
-
-def start():
-	GL1
-	RL2
-	time.sleep(5)
-	OL1
-	OL2
-	time.sleep(5)
-	GL2
-	RL1
-	time.sleep(5)
-
-while True:
-	start
-	continue
-
-
-=======
-import RPi.GPIO as GPIO
-import time
-
-def RL1():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(18,GPIO.OUT)
-	GPIO.output(18,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(18,GPIO.LOW)
-
-def RL2():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(17,GPIO.OUT)
-	GPIO.output(17,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(17,GPIO.LOW)
-
-def OL1():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(22,GPIO.OUT)
-	GPIO.output(22,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(22,GPIO.LOW)
-
-def OL2():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(23,GPIO.OUT)
-	GPIO.output(23,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(23,GPIO.LOW)
-	
-def GL1():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(6,GPIO.OUT)
-	GPIO.output(6,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(6,GPIO.LOW)
-	
-def GL2():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(12,GPIO.OUT)
-	GPIO.output(12,GPIO.HIGH)
-	time.sleep(5)
-	GPIO.output(12,GPIO.LOW)
-
-def start():
-	GL1
-	RL2
-	time.sleep(5)
-	OL1
-	OL2
-	time.sleep(5)
-	GL2
-	RL1
-	time.sleep(5)
-
-while True:
-	start
-	continue
-
-
->>>>>>> origin/master
+from multiprocessing import Process 	#Load a library called "multiprocessing" and import the commands from "Process"
+import sys								#Import the "sys" commands
+import RPi.GPIO as GPIO					#Import the "RPi.GPIO" commands that allow us to utilise the pins on the RaspberryPi
+import time								#Import the ability to use .time commands
+def Left():								#Create a definition that makes it easy to load up all the commands using one word rather than rewrite the code
+	GPIO.setmode(GPIO.BCM)				#Referring to the pins on the Pi
+	GPIO.setwarnings(False)				#Turn off the warnings
+	GPIO.setup(23,GPIO.OUT)				#Set the 23rd pin as an output (Green)
+	GPIO.output(23,GPIO.High)			#Turns on the LED that is connected to the pin
+	time.sleep(5)						#LED turns on for 5 seconds
+	GPIO.output(23,GPIO.Low)			#Turns off the LED
+	GPIO.setup(18,GPIO.OUT)				#Set the 18th pin as an output (Orange)
+	GPIO.output(18,GPIO.High)			#Turns on the LED that is connected to the pin
+	time.sleep(2)						#LED turns on for 2 seconds
+	GPIO.output(18,GPIO.Low)			#Turns off the LED
+	GPIO.setup(25,GPIO.OUT)				#Set the 25th pin as an output (Red)
+	GPIO.setup(25,GPIO.High				#Turns on the LED that is connected to the pin
+	time.sleep(7)						#LED turns on for 5 seconds
+	GPIO.setup(25,GPIO.Low)				#Turns off the LED
+	pass
+def Right():							#Create a definition that makes it easy to load up all the commands using one word rather than rewrite the code
+	GPIO.setmode(GPIO.BCM)				#Referring to the pins on the Pi
+	GPIO.setwarnings(False)				#Turn off the warnings
+	GPIO.setup(12,GPIO.OUT)				#Set the 12th pin as an output (Red)
+	GPIO.output(12,GPIO.High)			#Turns on the LED that is connected to the pin
+	time.sleep(7)						#LED turns on for 7 seconds
+	GPIO.output(12,GPIO.Low)			#Turns off the LED
+	GPIO.setup(17,GPIO.OUT)				#Set the 17th pin as an output (Green)
+	GPIO.output(17,GPIO.High)			#Turns on the LED that is connected to the pin
+	time.sleep(5)						#LED turns on for 5 seconds
+	GPIO.output(17,GPIO.Low)			#Turns off the LED
+	GPIO.setup(22,GPIO.OUT)				#Set the 22nd pin as an output (Orange)
+	GPIO.setup(22,GPIO.High)			#Turns on the LED that is connected to the pin
+	time.sleep(2)						#LED turns on for 2 seconds
+	GPIO.setup(22,GPIO.Low)				#Turns off the LED
+	pass
+if __name__ == '__main__':				#Allows us to run two or more commands at the same time
+	while True:							#The code is executed while the statement is True
+		Process(target=Left).start()	#Loads the "Left" process that we made before and starts it
+		Process(target=Right).start()	#Loads the "Right" process that we made before and starts it
+		time.sleep(14)					#Waits 14 seconds before proceeding to the next command
+		continue						#Goes back to the start of the 'While True' command
